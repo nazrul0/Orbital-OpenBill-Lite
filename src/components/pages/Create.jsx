c34dd82c211fc0fe1226bf71033d82ee72c19e0d
Nazrul Syahmi <nazrulsyahmi00@gmail.com>
2022-05-30 00:09:22 +0800

    updated create page

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -8,6 +8,7 @@ function Create() {
     <div>
       <PageTitle title="Create" />
       <div className="createContainer">
+        <h3>What would you like to propose today?</h3>
         <div className="createBody">
           <CreateCard className="createType" type="OpenQuestion" />
           <CreateCard className="createType" type="OpenMotion" />
