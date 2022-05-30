d63aef60c69d093f1b33445b820835e5b688fb9d
INDR <indraneel.paranjape@gmail.com>
2022-05-30 22:13:00 +0800

    mongodb connection- unresolved bugs

--- Backend/routes/user_routes.js
+++ Backend/routes/user_routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const usersControllers = require('../controllers/users_controllers');
+const mongoConnection = require('../mongo')
 
 router.get("/apitest", usersControllers.apitest);
 
 router.post("/signup", usersControllers.signup);
+router.post("/mongoTest", mongoConnection.createUser);
 
 module.exports = router;
