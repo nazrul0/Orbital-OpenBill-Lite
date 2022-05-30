e59ee2bd0d6ced1306929fb7752ef3a0816ba916
INDR <indraneel.paranjape@gmail.com>
2022-05-30 17:44:50 +0800

    using state to track user-entered information

--- Frontend/src/components/pages/Login.jsx
+++ Frontend/src/components/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import PageTitle from "../PageTitle";
 import './Login.css';
 import Input from '../templates/input.js';
@@ -7,17 +7,29 @@ import { VALIDATOR_EMAIL } from "../util/validators.js";
 
 
 function Login() {
-  // some logic
-  let entered_text = '';
+  // destructuring the standard returns of useState- 1.the latest state 2. function to update the state
+  // inside the useState call specify the initialisation state
+  const [enteredEmail, updateEnteredEmail] = useState('');
+  const [enteredPassword, updateEnteredPassword] = useState('');
 
-  const textChangeHandler = event => {
-    entered_text = event.target.value;
-    console.log(entered_text);
+  const emailChangeHandler = event => {
+    updateEnteredEmail(event.target.value);
+    console.log(enteredEmail);
+  };
+
+  const passwordChangeHandler = event => {
+    updateEnteredPassword(event.target.value);
   };
 
   const submitHandler = event => {
     event.preventDefault();
-    console.log("test");
+    
+    const new_user = {
+      id: enteredEmail,
+      password: enteredPassword
+    }
+
+    console.log(new_user);
   }
   
   // the element to return
@@ -31,9 +43,10 @@ function Login() {
             id = "email"
             type = 'email'
             label = 'E-mail'
+            value = {enteredEmail}
             validators = {[VALIDATOR_EMAIL()]}
             errortext = "Please enter a valid email address"
-            onChange = {textChangeHandler}
+            onChange = {emailChangeHandler}
           >
           </Input>
 
@@ -42,8 +55,9 @@ function Login() {
             id = "password"
             type = 'text'
             label = 'Password'
+            value = {enteredPassword}
             errortext = "Please enter a valid password"
-            onChange = {textChangeHandler}
+            onChange = {passwordChangeHandler}
           >
           </Input>
 
