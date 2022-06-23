c771c1011b470d4e7cd4038a1b9a0574a03b6da5
Nazrul Syahmi <nazrulsyahmi00@gmail.com>
2022-06-23 11:15:47 +0800

    added faq questions

--- Frontend/src/components/FaqData.js
+++ Frontend/src/components/FaqData.js
@@ -1,29 +1,30 @@
 const Data = [
   {
-    question: "What is the question?",
+    question: "What is OpenBill?",
     answer: "This is the answer.",
   },
   {
-    question:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc augue, ultricies at bibendum at, vehicula nec nisl?",
+    question: "How do user submissions work on OpenBill?",
     answer:
       "Ut suscipit aliquam rutrum. Ut vitae neque dictum, elementum eros tincidunt, luctus massa. Vestibulum auctor, nulla quis aliquam porta, sem nulla congue orci, at dictum elit ante quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu molestie quam. Integer at mattis neque, sed eleifend ex. Sed sit amet neque turpis. Sed placerat at tortor at ullamcorper. Aliquam ligula lacus, vestibulum in tincidunt nec, lacinia a turpis.",
   },
   {
-    question:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc augue, ultricies at bibendum at, vehicula nec nisl?",
+    question: "What are the different types of proposals?",
     answer:
       "Ut suscipit aliquam rutrum. Ut vitae neque dictum, elementum eros tincidunt, luctus massa. Vestibulum auctor, nulla quis aliquam porta, sem nulla congue orci, at dictum elit ante quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu molestie quam. Integer at mattis neque, sed eleifend ex. Sed sit amet neque turpis. Sed placerat at tortor at ullamcorper. Aliquam ligula lacus, vestibulum in tincidunt nec, lacinia a turpis.",
   },
   {
-    question:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc augue, ultricies at bibendum at, vehicula nec nisl?",
+    question: "Are there any plans to implement new features?",
     answer:
       "Ut suscipit aliquam rutrum. Ut vitae neque dictum, elementum eros tincidunt, luctus massa. Vestibulum auctor, nulla quis aliquam porta, sem nulla congue orci, at dictum elit ante quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu molestie quam. Integer at mattis neque, sed eleifend ex. Sed sit amet neque turpis. Sed placerat at tortor at ullamcorper. Aliquam ligula lacus, vestibulum in tincidunt nec, lacinia a turpis.",
   },
   {
-    question:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nunc augue, ultricies at bibendum at, vehicula nec nisl?",
+    question: "Who is the team behind OpenBill?",
+    answer:
+      "Ut suscipit aliquam rutrum. Ut vitae neque dictum, elementum eros tincidunt, luctus massa. Vestibulum auctor, nulla quis aliquam porta, sem nulla congue orci, at dictum elit ante quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu molestie quam. Integer at mattis neque, sed eleifend ex. Sed sit amet neque turpis. Sed placerat at tortor at ullamcorper. Aliquam ligula lacus, vestibulum in tincidunt nec, lacinia a turpis.",
+  },
+  {
+    question: "Where can I contact the team for any further queries?",
     answer:
       "Ut suscipit aliquam rutrum. Ut vitae neque dictum, elementum eros tincidunt, luctus massa. Vestibulum auctor, nulla quis aliquam porta, sem nulla congue orci, at dictum elit ante quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu molestie quam. Integer at mattis neque, sed eleifend ex. Sed sit amet neque turpis. Sed placerat at tortor at ullamcorper. Aliquam ligula lacus, vestibulum in tincidunt nec, lacinia a turpis.",
   },
