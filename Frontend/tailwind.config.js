fa5e230c05b2d78e99300fbaa7e18b6fde5cf2e0
INDR <indraneel.paranjape@gmail.com>
2022-06-27 18:10:21 +0800

    changed tw config file

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      main: ['"Red Hat Text"', "Helvetica", "sans-serif"],
+      title: ['"Space Grotesk"', "Red Hat Text", "Helvetica", "sans-serif"],
+      logo: ['"Barlow"', "Helvetica", "sans-serif"],
+    },
+
+    extend: {
+      fontSize: {
+        md: "1.2rem",
+      },
+
+      colors: {
+        turquoise: {
+          500: "#73FDEA",
+          400: "#16E7CF",
+        },
+      },
+    },
   },
   plugins: [],
 }
