358b58f6e9e8db5d21de42cdd0af8e57766ccfb1
Nazrul Syahmi <nazrulsyahmi00@gmail.com>
2022-06-27 16:58:44 +0800

    added missing module for deployment

---
+++ Frontend/src/config/connectionData.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCnBqb5I3yxL_pRKDxgDV74hie8Z20zdzc",
+  authDomain: "openbill-v2.firebaseapp.com",
+  projectId: "openbill-v2",
+  storageBucket: "openbill-v2.appspot.com",
+  messagingSenderId: "162333827019",
+  appId: "1:162333827019:web:d88bb24329b2eb3348a04e",
+  measurementId: "G-6RVKTHCJ5Y",
+};
