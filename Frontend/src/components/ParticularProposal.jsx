fdc5e7a117aef5c3e764e5e2ac5869cba82374a9
INDR <indraneel.paranjape@gmail.com>
2022-07-07 20:17:19 +0800

    displays the particular proposal's content when clicked from Proposals Home

--- Frontend/src/components/ParticularProposal.jsx
+++ Frontend/src/components/ParticularProposal.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Proposal(props) {
+
+  const location = useLocation();
+  const {curr} = location.state;
+  
   return (
     <div>
-      <h4>A proposal</h4>
+      <h4>{curr.Title}</h4>
+      <h5>By {curr.displayName}</h5>
+      <p>
+        {curr.Content}
+      </p>
     </div>
   );
 }
