20e15fdcc5cf2ce231e06d497961e08bd1fb6093
INDR <indraneel.paranjape@gmail.com>
2022-07-18 17:49:01 +0800

    updating styling edit

--- Frontend/src/pages/Create.jsx
+++ Frontend/src/pages/Create.jsx
@@ -13,8 +13,8 @@ function Create() {
         <PageTitle title="Create" />
         <div className="grid lg:grid-cols-3 grid-cols-5 justify-items-center mx-auto">
           
-          <div className="mt-8 lg:col-span-2 lg:col-start-1 cols-start-1 col-span-4">
-            <img src={constellation} alt="Openbill Constellation" className="w-full -mb-60"/>
+          <div className="mt-8 lg:col-span-3 lg:col-start-1 lg:mx-48 cols-start-1 col-span-5 mx-8">
+            <img src={constellation} alt="Openbill Constellation" className="w-full -mb-64"/>
           </div>
           
           <div className="lg:col-start-2 lg:col-span-1 col-start-2 col-span-3 mb-4">
@@ -23,11 +23,11 @@ function Create() {
               <img className="object-fit col-start-2 col-span-1 bg-indigo-400 rounded" src={gavel} alt="Option Icon" />
             </Link>
           </div>
-
+{/*
           <div className="mt-8 lg:col-start-2 lg:col-span-2 col-start-2 col-span-4">
             <img src={constellation} alt="Openbill Constellation" className="w-full -mb-60"/>
           </div>
-
+  */}
           <div className="lg:col-start-2 lg:col-span-1 col-start-2 col-span-3 mt-4">
             <Link className="grid grid-cols-2 bg-black text-white place-items-center rounded shadow-xl" to="/SubmitOpenQuestion">
               <h3 className="font-main">OpenQuestion</h3>
