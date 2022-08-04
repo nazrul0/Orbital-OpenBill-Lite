e65e5290638fe8325f8cd6199f79f8dd9b588a26
INDR <indraneel.paranjape@gmail.com>
2022-08-04 12:42:25 +0800

    readability edits for article and proposals (width of screen occupied)

--- Frontend/src/components/ParticularArticle.js
+++ Frontend/src/components/ParticularArticle.js
@@ -11,14 +11,14 @@ function ParticularArticle() {
   return (
     <div className="bg-slate-100">
       <PageTitle title="Article" />
-      <div className="mainArticleContainer">
-        <div className="particularArticleContainer">
-          <section className="particularArticleHeader bg-white">
+      <div className="grid grid-cols-6">
+        <div className="col-start-2 col-span-4">
+          <section className="particularArticleHeader bg-white mb-6 mt-2">
             <h2 className="particularArticleTitle">{curr.Title}</h2>
-            <h4 className="particularArticleOwner">By {curr.DisplayName}</h4>
+            <h4 className="particularArticleOwner pt-4">By {curr.DisplayName}</h4>
           </section>
 
-          <section className="particularArticleContent bg-white">
+          <section className="particularArticleContent bg-white mb-10">
             <QuillEditor readMode={true} payload={JSON.parse(curr.Content)} />
           </section>
         </div>
